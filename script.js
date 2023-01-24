6b61e84ded0e302e6ff700ea26666714511c7484
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-24 21:42:11 +0100

    js_string methods_length

---
+++ script.js
@@ -0,0 +1,14 @@
+const firstName = "Nikodem";
+const lastName = "Gingerbread";
+console.log(firstName.length, lastName.length);
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(fullName.length);
+
+
+
+/* Erstelle zwei Variablen firstName und lastName.
+Zeige die Länge der beiden Variablen in der Konsole an.
+Erstelle noch eine Variable fullName, die die Summe der beiden Variablen ist.
+Zeige die Länge der Variable fullName in der Konsole an. */
